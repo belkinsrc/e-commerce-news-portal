b67c08ce17b23efcd3b5cd72e8db798d52044058
Nikita Belkin <belkinsrc@gmail.com>
2024-06-05 14:25:17 +0300

    feat: make script for 'header-menu' component

--- src/scripts/components/menu.js
+++ src/scripts/components/menu.js
@@ -1,75 +1,87 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('[data-header]');
-  const menu = header?.querySelector('[data-menu]');
-  const burger = header?.querySelector('[data-burger-menu]');
-  const menuLinks = header?.querySelectorAll('a');
-  const body = document.body;
-  const focusableElementsString =
-    'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
-  const focusableElements = header?.querySelectorAll(focusableElementsString);
-  let firstFocusableElement = focusableElements[0];
-  let lastFocusableElement = focusableElements[focusableElements.length - 1];
+export class MenuModel {
+  constructor() {
+    this.header = document.querySelector('[data-header]');
+    this.menu = this.header?.querySelector('[data-menu]');
+    this.burger = this.header?.querySelector('[data-burger-menu]');
+    this.menuLinks = this.header?.querySelectorAll('a');
+    this.body = document.body;
+    this.focusableElementsString =
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
+    this.focusableElements = this.header?.querySelectorAll(this.focusableElementsString);
+    this.firstFocusableElement = this.focusableElements[0];
+    this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1];
+    this.#init();
+  }
 
-  burger?.addEventListener('click', () => {
-    burger.classList.toggle('burger-menu--expanded');
-    if (burger.classList.contains('burger-menu--expanded')) {
-      burger.setAttribute('aria-expanded', true);
-      menu?.classList.add('menu--expanded');
-      disableScroll();
-      addHandlerToLockFocusWithInHeader();
-    } else {
-      burger.setAttribute('aria-expanded', false);
-      menu?.classList.remove('menu--expanded');
-      enableScroll();
-      removeHandlerToLockFocusWithInHeader();
-    }
-  });
+  #init() {
+    this.#handleBurgerClick();
+    this.#handleMenuLinksClick();
+  }
 
-  menuLinks?.forEach((link) => {
-    link.addEventListener('click', () => {
-      burger.classList.remove('burger-menu--expanded');
-      menu.classList.remove('menu--expanded');
-      enableScroll();
+  #handleBurgerClick() {
+    this.burger?.addEventListener('click', () => {
+      this.burger.classList.toggle('burger-menu--expanded');
+      if (this.burger.classList.contains('burger-menu--expanded')) {
+        this.burger.setAttribute('aria-expanded', true);
+        this.menu?.classList.add('menu--expanded');
+        this.#disableScroll();
+        this.#addHandlerToLockFocusWithInHeader();
+      } else {
+        this.burger.setAttribute('aria-expanded', false);
+        this.menu?.classList.remove('menu--expanded');
+        this.#enableScroll();
+        this.#removeHandlerToLockFocusWithInHeader();
+      }
     });
-  });
+  }
 
-  function disableScroll() {
+  #handleMenuLinksClick() {
+    this.menuLinks?.forEach((link) => {
+      link.addEventListener('click', () => {
+        this.burger?.classList.remove('burger-menu--expanded');
+        this.menu?.classList.remove('menu--expanded');
+        this.#enableScroll();
+      });
+    });
+  }
+
+  #disableScroll() {
     let scrollPosition = window.scrollY;
-    body.dataset.scroll = scrollPosition;
-    body.classList.add('disable-scroll');
-    body.style.top = -scrollPosition + 'px';
+    this.body.dataset.scroll = scrollPosition;
+    this.body.classList.add('disable-scroll');
+    this.body.style.top = -scrollPosition + 'px';
   }
 
-  function enableScroll() {
-    let scrollPosition = parseInt(body.dataset.scroll, 10);
-    body.style.top = 'auto';
-    body.classList.remove('disable-scroll');
+  #enableScroll() {
+    let scrollPosition = parseInt(this.body.dataset.scroll, 10);
+    this.body.style.top = 'auto';
+    this.body.classList.remove('disable-scroll');
     window.scroll({ top: scrollPosition, left: 0 });
-    body.removeAttribute('data-scroll');
+    this.body.removeAttribute('data-scroll');
   }
 
-  function lockFocusWithInHeader(e) {
+  #lockFocusWithInHeader() {
     const isTabPressed = e.key === 'Tab' || e.keyCode === 9;
     if (!isTabPressed) return;
 
     if (e.shiftKey) {
-      if (document.activeElement === firstFocusableElement) {
-        lastFocusableElement.focus();
+      if (document.activeElement === this.firstFocusableElement) {
+        this.lastFocusableElement.focus();
         e.preventDefault();
       }
     } else {
-      if (document.activeElement === lastFocusableElement) {
-        firstFocusableElement.focus();
+      if (document.activeElement === this.lastFocusableElement) {
+        this.firstFocusableElement.focus();
         e.preventDefault();
       }
     }
   }
 
-  function addHandlerToLockFocusWithInHeader() {
-    header?.addEventListener('keydown', lockFocusWithInHeader);
+  #addHandlerToLockFocusWithInHeader() {
+    this.header?.addEventListener('keydown', this.#lockFocusWithInHeader);
   }
 
-  function removeHandlerToLockFocusWithInHeader() {
-    header?.removeEventListener('keydown', lockFocusWithInHeader);
+  #removeHandlerToLockFocusWithInHeader() {
+    this.header?.removeEventListener('keydown', this.#lockFocusWithInHeader);
   }
-});
+}
