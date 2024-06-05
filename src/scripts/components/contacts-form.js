b5462c8627ede75b5e4313e1c40a76ef20ae548a
Nikita Belkin <belkinsrc@gmail.com>
2024-06-05 14:27:55 +0300

    feat: make validation for 'contacts-form' component

---
+++ src/scripts/components/contacts-form.js
@@ -0,0 +1,87 @@
+export class ContactsFormModel {
+  constructor() {
+    this.form = document.querySelector('[data-contacts-form]');
+    this.inputList = Array.from(
+      this.form?.querySelectorAll('.contacts-form__input')
+    );
+    this.buttonElement = this.form?.querySelector('.contacts-form__button');
+    this.formErrorElement = this.form?.querySelector('#empty-error');
+    this.#startValidation();
+  }
+
+  #startValidation() {
+    this.#toggleButton();
+
+    this.form?.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      if (this.#hasInvalidInput()) {
+        this.#formError();
+      }
+    });
+
+    this.inputList?.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this.#checkInputValidity(inputElement);
+        this.#toggleButton();
+      });
+    });
+  }
+
+  #checkInputValidity(inputElement) {
+    if (inputElement.validity.patternMismatch) {
+      inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    } else {
+      inputElement.setCustomValidity(this.#checkLengthMismatch(inputElement));
+    }
+    if (!inputElement.validity.valid) {
+      inputElement.setAttribute('aria-invalid', true);
+      this.#showErrorMessage(inputElement, inputElement.validationMessage);
+    } else {
+      inputElement.setAttribute('aria-invalid', false);
+      this.#clearErrorMessage(inputElement);
+    }
+  }
+
+  #checkLengthMismatch(inputElement) {
+    if (inputElement.type !== 'text') return '';
+
+    if (inputElement.validity.tooShort) {
+      return inputElement.validationMessage;
+    }
+    return '';
+  }
+
+  #showErrorMessage(inputElement, errorMessage) {
+    const errorElement = document.getElementById(`${inputElement.id}-error`);
+    errorElement.textContent = errorMessage;
+    inputElement.classList.add('contacts-form__input--error');
+  }
+
+  #clearErrorMessage(inputElement) {
+    const errorElement = document.getElementById(`${inputElement.id}-error`);
+    errorElement.textContent = '';
+    inputElement.classList.remove('contacts-form__input--error');
+  }
+
+  #toggleButton() {
+    if (this.#hasInvalidInput()) {
+      this.buttonElement?.setAttribute('aria-disabled', true);
+      this.buttonElement?.classList.add('contacts-form__button--disabled');
+    } else {
+      this.buttonElement?.removeAttribute('aria-disabled');
+      this.buttonElement?.classList.remove('contacts-form__button--disabled');
+      this.formErrorElement.textContent = '';
+    }
+  }
+
+  #formError() {
+    this.formErrorElement.textContent = 'Заполните все поля для отправки формы.';
+  }
+
+  #hasInvalidInput() {
+    return this.inputList?.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-import './components/menu.js';
+import { MenuModel } from './components/menu.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  runModels();
+});
+
+function runModels() {
+  const body = document.body;
+
+  switch (body.id) {
+    case 'contacts':
+      import('./components/contacts-form.js')
+        .then((module) => new module.ContactsFormModel());
+    default:
+      new MenuModel();
+  } 
+}
